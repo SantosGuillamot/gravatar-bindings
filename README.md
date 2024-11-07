9e616894f3958860b68f6830bdded3699553945d
Mario Santos <santosguillamot@gmail.com>
2024-11-07 16:45:32 +0100

    Init plugin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gravatar-bindings
+# Gravatar Bindings
+
+In this repository, I aim to explore the possibility of creating about me pages using core blocks connected directly to Gravatar. To handle that, I will create a block bindings source, which will serve to identify potential blockers or missing functionalities.
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Gravatar Bindings
+ * Version:           1.0.0
+ * Requires at least: 6.7
+ * Requires PHP:      5.6
+ * Description:       Plugin created for testing the block bindings API
+ * Author:            Mario Santos
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       santosguillamot
+ */
