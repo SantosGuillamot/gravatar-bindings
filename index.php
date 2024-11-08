3cb58ea7df623a204030ae4fbd2c56f43018f4f7
Mario Santos <santosguillamot@gmail.com>
2024-11-08 11:02:36 +0100

    Register source in the server

--- index.php
+++ index.php
@@ -17,3 +17,31 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable( __DIR__ );
 $dotenv->load();
+
+/**
+ * Register block bindings source.
+ */
+function gravatar_register_block_bindings_source() {
+	register_block_bindings_source(
+		'santosguillamot/gravatar',
+		array(
+			'label'              => __( 'Gravatar', 'santosguillamot' ),
+			'get_value_callback' => function ( $source_args ) {
+				// Fetch data from Gravatar API.
+				$args = array();
+				// Add auth headers if API key is set.
+				if ( isset( $_ENV['GRAVATAR_API_KEY'] ) ) {
+					$args['headers'] = array(
+						'Authorization' => 'Bearer ' . $_ENV['GRAVATAR_API_KEY'],
+					);
+				}
+				$response = wp_remote_get( 'https://api.gravatar.com/v3/profiles/' . $source_args['id'], $args );
+				$body = wp_remote_retrieve_body( $response );
+				$data = json_decode( $body );
+				$field = $source_args['field'];
+				return $data->$field;
+			},
+		)
+	);
+}
+add_action( 'init', 'gravatar_register_block_bindings_source' );
