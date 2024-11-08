28e8e44d88df20823612beeca8b70fe62aafbd11
Mario Santos <santosguillamot@gmail.com>
2024-11-08 10:56:43 +0100

    Add support for ENV variables

--- index.php
+++ index.php
@@ -10,3 +10,10 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       santosguillamot
  */
+
+// Load Composer's autoload file.
+require_once __DIR__ . '/vendor/autoload.php';
+// Load Enviroment Variables.
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable( __DIR__ );
+$dotenv->load();
